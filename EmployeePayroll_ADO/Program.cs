9efd0edf274635ff44ac91ffdaea7e35be726044
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-21 17:36:52 +0530

    [Elavarasu] UC1-Connecting Database

---
+++ EmployeePayroll_ADO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayroll_ADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeRepo getMethod = new EmployeeRepo();
+            getMethod.Connectivity();
+        }
+    }
+}
